c22401c97a79ecdaeac6ae05b2f117f094c040fb
agastonsosa <138701662+agastonsosa@users.noreply.github.com>
2023-11-25 02:59:58 +0000

    layour, app y flux changed

--- src/front/js/layout.js
+++ src/front/js/layout.js
@@ -3,31 +3,44 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 import { BackendURL } from "./component/backendURL";
 
-import { Home } from "./pages/home";
-import { Demo } from "./pages/demo";
-import { Single } from "./pages/single";
+import { Home } from "./pages/Home";
 import injectContext from "./store/appContext";
-
-import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Login } from "./pages/Login";
+import { Signup } from "./pages/Signup";
+import PasswordRecovery from "./pages/PasswordRecovery.js";
+import PasswordReset from "./pages/PasswordReset";
+import Dashboard from "./pages/Dashboard";
+import Ingredients from "./pages/Ingredients";
+import { Profile } from "./pages/Profile";
+import Products from "./pages/Products";
+import Recipes from "./pages/Recipes";
+import SingleRecipe from "./pages/SingleRecipe";
+import { EditProfile } from "./pages/EditProfile";
 
-//create your first component
+
 const Layout = () => {
-    //the basename is used when your project is published in a subdirectory and not in the root of the domain
-    // you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
     const basename = process.env.BASENAME || "";
 
-    if(!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL/ >;
+    if (!process.env.BACKEND_URL || process.env.BACKEND_URL == "") return <BackendURL />;
 
     return (
         <div>
             <BrowserRouter basename={basename}>
                 <ScrollToTop>
-                    <Navbar />
                     <Routes>
                         <Route element={<Home />} path="/" />
-                        <Route element={<Demo />} path="/demo" />
-                        <Route element={<Single />} path="/single/:theid" />
+                        <Route element={<Login />} path="/login" />
+                        <Route element={<PasswordRecovery />} path="/passwordrecovery" />
+                        <Route element={<PasswordReset />} path="/passwordreset/:reset_token" />
+                        <Route element={<Signup />} path="/signup" />
+                        <Route element={<Dashboard />} path="/dashboard" />
+                        <Route element={<Ingredients />} path="/dashboard/ingredients" />
+                        <Route element={<Profile />} path="/dashboard/profile" />
+                        <Route element={<EditProfile />} path="/dashboard/edit-profile" />
+                        <Route element={<Products />} path="/dashboard/products" />
+                        <Route element={<Recipes />} path="/dashboard/recipes" />
+                        <Route element={<SingleRecipe />} path="/dashboard/recipes/:recipe_id" />
                         <Route element={<h1>Not found!</h1>} />
                     </Routes>
                     <Footer />
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -2,51 +2,120 @@ const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
 			message: null,
-			demo: [
-				{
-					title: "FIRST",
-					background: "white",
-					initial: "white"
-				},
-				{
-					title: "SECOND",
-					background: "white",
-					initial: "white"
-				}
-			]
+			user: null,
+			userLoggedIn: null,
+			profile:[],
 		},
+
 		actions: {
 			// Use getActions to call a function within a fuction
 			exampleFunction: () => {
 				getActions().changeColor(0, "green");
 			},
 
+			signup: async (name, last_name, email, password, address) => {
+				try {
+					const resp = await fetch(process.env.BACKEND_URL + "/signup", {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+						},
+						body: JSON.stringify({
+							name: name,
+							last_name: last_name,
+							email: email,
+							password: password,
+							address: address,
+						}),
+					});
+
+					if (!resp.ok) {
+						const data = await resp.json();
+						throw new Error(data.message);
+					}
+
+					const data = await resp.json();
+					// Guardar el token en el localStorage
+					localStorage.setItem("jwt-token", data.token);
+
+					return data;
+				} catch (error) {
+					throw new Error(`Error during signup: ${error.message}`);
+				}
+			},
+
+
+			login: async (email, password) => {
+				try {
+					const resp = await fetch(process.env.BACKEND_URL + "/login", {
+						method: "POST",
+						headers: { "Content-Type": "application/json" },
+						body: JSON.stringify({ email: email, password: password }),
+					});
+
+					if (!resp.ok) {
+						console.log("Response status:", resp.status);
+						console.log("Response status text:", resp.statusText);
+
+						if (resp.status === 401) {
+							throw new Error("Invalid credentials");
+						} else if (resp.status === 400) {
+							throw new Error("Invalid email or password format");
+						} else {
+							throw new Error("Error during login request");
+						}
+					}
+
+					const data = await resp.json();
+
+					localStorage.setItem("jwt-token", data.token);
+					setStore({ userLoggedIn: localStorage.getItem("jwt-token") })
+
+					return data;
+				} catch (error) {
+					throw new Error(`Error during login: ${error.message}`);
+				}
+			},
+
+
+
+			logout: async () => {
+				try {
+					const resp = await fetch(process.env.BACKEND_URL + "/logout", {
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+							Authorization: `Bearer ${localStorage.getItem("jwt-token")}`,
+						},
+					});
+
+					if (!resp.ok) {
+						throw new Error("Error during logout request");
+					}
+
+					localStorage.removeItem("jwt-token");
+
+					setStore({ userLoggedIn: null });
+
+					return true;
+				} catch (error) {
+					throw new Error(`Error during logout: ${error.message}`);
+				}
+			},
+
+			
 			getMessage: async () => {
-				try{
+				try {
 					// fetching data from the backend
 					const resp = await fetch(process.env.BACKEND_URL + "/api/hello")
 					const data = await resp.json()
 					setStore({ message: data.message })
 					// don't forget to return something, that is how the async resolves
 					return data;
-				}catch(error){
+				} catch (error) {
 					console.log("Error loading message from backend", error)
 				}
 			},
-			changeColor: (index, color) => {
-				//get the store
-				const store = getStore();
-
-				//we have to loop the entire demo array to look for the respective index
-				//and change its color
-				const demo = store.demo.map((elm, i) => {
-					if (i === index) elm.background = color;
-					return elm;
-				});
-
-				//reset the global store
-				setStore({ demo: demo });
-			}
 		}
 	};
 };
